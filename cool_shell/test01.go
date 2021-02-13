b551c7c4c8d9fdd412e6ab2bdf0bb0b729e5a491
Kamakura.Yx <yuxiang204@yeah.net>
2021-02-13 22:39:07 +0800

    deep equal and full slice expression
    Signed-off-by: Kamakura.Yx <yuxiang204@yeah.net>

---
+++ cool_shell/test01.go
@@ -0,0 +1,42 @@
+package cool_shell
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+func Test01() {
+	path := []byte("AAAA/BBBBBBBBB")
+	sepIndex := bytes.IndexByte(path, '/')
+	dir1 := path[:sepIndex]
+	dir2 := path[sepIndex+1:]
+	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAA
+	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => BBBBBBBBB
+	dir1 = append(dir1, "suffix"...)
+	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAAsuffix
+	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => uffixBBBB
+}
+
+func Test01Fixed() {
+	path := []byte("AAAA/BBBBBBBBB")
+	sepIndex := bytes.IndexByte(path, '/')
+	dir1 := path[:sepIndex:sepIndex]
+	dir2 := path[sepIndex+1:]
+	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAA
+	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => BBBBBBBBB
+	dir1 = append(dir1, "suffix"...)
+	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAAsuffix
+	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => uffixBBBB
+}
+
+func TestDeepEqual() {
+
+	type data struct {
+	}
+
+	v1 := data{}
+	v2 := data{}
+	fmt.Println("v1 == v2:", reflect.DeepEqual(v1, v2))
+	//prints: v1 == v2: true
+}
